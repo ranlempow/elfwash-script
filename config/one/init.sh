374574266854e15f99c93dface04ac7988fd53a2
ranlempow <ranlempow@hotmail.com>
2016-05-17 23:33:06 +0800

    linux tested OK

--- config/one/init.sh
+++ config/one/init.sh
@@ -8,14 +8,14 @@ fi
 APPSDIR=${BINDIR}/apps
 if [ ! -d "${APPSDIR}" ]; then
     mkdir "${APPSDIR}"
-    echo '#!/bin/bash'                                               > "${APPDIR}\set-env.sh"
-    echo 'dp0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"'  >> "${APPDIR}\set-env.sh"
-    echo 'for FILE in ${dp0}/*/set-env.sh'                          >> "${APPDIR}\set-env.sh"
-    echo 'do'                                                       >> "${APPDIR}\set-env.sh"
-    echo '  if [ -f $FILE ]; then'                                  >> "${APPDIR}\set-env.sh"
-    echo '    source $FILE'                                         >> "${APPDIR}\set-env.sh"
-    echo '  fi'                                                     >> "${APPDIR}\set-env.sh"
-    echo 'done'                                                     >> "${APPDIR}\set-env.sh"
+    echo '#!/bin/bash'                                               > "${APPSDIR}/set-env.sh"
+    echo 'dp0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"'  >> "${APPSDIR}/set-env.sh"
+    echo 'for FILE in ${dp0}/*/set-env.sh'                          >> "${APPSDIR}/set-env.sh"
+    echo 'do'                                                       >> "${APPSDIR}/set-env.sh"
+    echo '  if [ -f $FILE ]; then'                                  >> "${APPSDIR}/set-env.sh"
+    echo '    source $FILE'                                         >> "${APPSDIR}/set-env.sh"
+    echo '  fi'                                                     >> "${APPSDIR}/set-env.sh"
+    echo 'done'                                                     >> "${APPSDIR}/set-env.sh"
 
 fi
 
@@ -34,15 +34,18 @@ if [ ! -d ${NODEDIR}/node ]; then
     elif [ "$PLATFROM" == "Linux" ]; then
         NODEURL=https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
     fi
-    wget ${NODEURL}
     NODEFILE=${NODEURL##*/}
+    if [ -f ${NODEFILE} ]; then
+        rm ${NODEFILE}
+    fi
+    wget ${NODEURL}
     tar -Jpxvf ${NODEFILE} -C ${NODEDIR}
     mv ${NODEDIR}/${NODEFILE%.tar.xz} ${NODEDIR}/node
     rm ${NODEFILE}
-    echo '#!/bin/bash'                                               > "${NODEDIR}\set-env.sh"
-    echo 'dp0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"'  >> "${NODEDIR}\set-env.sh"
-    echo 'PATH=${dp0}/node:${PATH}'                                 >> "${NODEDIR}\set-env.sh"
-    echo 'PATH=${PROJECT_BASE}/node_modules/.bin:${PATH}'           >> "${NODEDIR}\set-env.sh"
+    echo '#!/bin/bash'                                               > "${NODEDIR}/set-env.sh"
+    echo 'dp0="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"'  >> "${NODEDIR}/set-env.sh"
+    echo 'PATH=${dp0}/node:${PATH}'                                 >> "${NODEDIR}/set-env.sh"
+    echo 'PATH=${PROJECT_BASE}/node_modules/.bin:${PATH}'           >> "${NODEDIR}/set-env.sh"
     
 fi
 
